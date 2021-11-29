74c2b586e31619519783f98816cdda1df5d0bddf
amaralvieira335 <93886588+amaralvieira335@users.noreply.github.com>
2021-11-30 00:23:32 +0100

    Add files via upload

---
+++ java1/main.js
@@ -0,0 +1,33 @@
+console.log('hallo');
+
+
+let name = ("Amaral");
+console.log(name)
+
+let age = "41";
+console.log(age);
+
+console.log(typeof age)
+
+let a = 4
+let b = 5
+var c = a + b;
+
+console. log("The sum of "+a+" and"+b+" is:"+c)
+
+function addNums(num1, num2) {
+    console. log(num1 + num2);
+}
+addNums(5,5);
+
+function addNums(num1, num2) {
+    console. log(num1 + num2);
+}
+addNums(5,5);
+
+console.log(5 - 3);
+console.log(2 + 2);
+console.log(2 * 6);
+console.log(8 / 2);
+
+
