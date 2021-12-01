89afb5d52638cfebd47013513c86991bf2c40cee
amaralvieira335 <93886588+amaralvieira335@users.noreply.github.com>
2021-12-01 21:37:14 +0100

    Add files via upload

---
+++ java2/script.js
@@ -0,0 +1,11 @@
+console.log("Hello Winc Academy students")
+
+//this is a large sum
+
+let multiplication = 1230941 * 1823794;
+console.log("multiplication:", multiplication);
+
+ let division = 637263 / 54;
+console.log("division:", division);
+
+/* Answer to 4 and 6 = JavaScript multiline comment has the same purpose as a single-line comment. JavaScript comment block starts with two symbols: (/)+(*). To end the comment, use these symbols again. Everything between these symbols will be treated as a comment */
