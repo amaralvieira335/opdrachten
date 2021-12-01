420ab1cabfc72ac6dc67700d8aaedfce4e4b6956
amaralvieira335 <93886588+amaralvieira335@users.noreply.github.com>
2021-12-02 00:02:14 +0100

    Add files via upload

---
+++ java3/script.js
@@ -0,0 +1,34 @@
+const jeLeeftijd = 18;
+
+if (jeLeeftijd > 18) {
+    document.write("je bent ouder dan 18 je mag naar binnen<br>");
+
+
+} if (jeLeeftijd == 18) {
+    document.write("je bent 18 je mag naar binnen <br>");
+
+
+
+} else if (jeLeeftijd < 18) {
+    document.write("het spijt me zeer maar je bent niet oud genoeg daarom wordt je niet toegelaten in ons pub<br>");
+}
+
+
+const isvrouw = true;
+
+
+if (isvrouw) {
+    document.write("je mag naar binnen het is ladies night<br>")
+
+} else {
+    document.write("je kan niet naar binnen want het is ladies night<br>")
+}
+
+const driverStatus = "bob";
+
+
+if (driverStatus == "dronkenlap") {
+    document.write(" jij bent de bob je mag rijden")
+} else {
+    document.write("je mag niet rijden want je hebt alcohol gedronken")
+}
