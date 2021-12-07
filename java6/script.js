c9f63e48418368fda7526ca219b437f67b132cc5
amaralvieira335 <93886588+amaralvieira335@users.noreply.github.com>
2021-12-07 18:38:01 +0100

    Add files via upload

---
+++ java6/script.js
@@ -0,0 +1,30 @@
+const maakPizza = function() {
+    
+    console.log ('los 7 gram gist op in 300ml lau water op')
+    console.log ('mix 1 eetlepen zeezout met 500 gram 00 bloem')
+    console.log ('mix de bloem met de gistoplossing ,kneed het deeg voor 10min lang en laat 1 uur reizen in een afgedekte kom')
+    console.log ('geef de pizzabodem vorm met een deegroller of fleswijn')
+    console.log ('beleg de pizzabodem eerst met saus en daarna de rest van de ingredienten')
+    console.log ('plaats de pizza in de oven voor 12min op 225 graden celsius')
+    console.log ('snij de pizza en eetsmakelijk')
+
+};
+maakPizza();
+maakPizza();
+
+
+const maakSushi = function() {
+    
+    console.log ('kook 1kg sushirijst en marineer het met mirin en laat affkoelen')
+    console.log ('leg 1 handjevol rijst op de zeewier blad verspreid het ')
+    console.log ('beleg de sushi naar keus')
+    console.log ('leg je zeewierblad op het bamboe matje')
+    console.log ('rol de sushi stevig dicht')
+    console.log ('snij de sushirol in 6 of 8 stukken maak het mes nat na elke keer snijden')
+    console.log ('eet de sushi op kamertemperatuur')
+
+};
+
+maakSushi();
+maakSushi();
+maakSushi();
