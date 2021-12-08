f4156b85541f63823bd45c90a24b084b883f4306
amaralvieira335 <93886588+amaralvieira335@users.noreply.github.com>
2021-12-08 18:40:50 +0100

    Add files via upload

---
+++ java7/script.js
@@ -0,0 +1,18 @@
+
+
+const verfen = function( kwastA , kwastB ,kwastC) {
+    
+    console.log ("The wall has been painted red")
+   
+    console.log ( "the wall has been painted " + kwastA )
+    console.log (kwastB)
+    console.log (kwastB + "and" + kwastC)
+    
+    
+    
+};
+
+
+verfen("green" ,"The south-east wall has been painted grey " , "The north wall has been painted orange")
+//the order of the arguments matter when calling the function!
+// if i switch them the sentence won`t make any sense
