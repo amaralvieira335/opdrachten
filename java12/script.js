052473c49e520b2a808855b49ef1c13c17d5d231
amaralvieira335 <93886588+amaralvieira335@users.noreply.github.com>
2021-12-22 21:22:36 +0100

    Add files via upload

---
+++ java12/script.js
@@ -0,0 +1,22 @@
+
+const color = ["geel", "blau", "rood", "oranje"];
+
+let i = 0;
+while (i < color.length) {
+   
+  console.log(color[i]); 
+  i++;
+}
+
+//for loop.
+const colors = ["geel", "blau", "rood", "oranje"];
+
+for (i = 0; i <colors.length; i++) {
+    console.log(colors[i]);
+}
+
+//foreach loop
+
+const array1 = ["geel", "blau", "rood", "oranje"];
+
+array1.forEach(element => console.log(element));
